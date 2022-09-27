def7b9e312ed49e983ff7aa5ca3ef5c7689522b4
Terogene Claudio <cterogene@gmail.com>
2022-09-27 09:27:27 -0500

    Started to design new responsive portfolio design for mobile and desktop.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,34 +5,29 @@ import obensonWork2 from "../../assets/images/obenson-work2.png"
 import { Link } from "react-router-dom";
 
 const Portfolio = () => {
-    return ( 
-        <>
-         <div className='project'>
-             <h1>My Projects</h1>
-            
-        </div>
-        <div className='works'>
-                <div className="work__section">
-        
-                    <div className="cards">
-                        <img src={obensonWork} alt="" />
-                        <Link to='/project'><button>See More</button></Link>
-                    </div>
+	
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
+			
+	
+					<div className="mb-10 sm:mb-0" >
+						<img
+							src={obensonWork}
+                           
+							className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
+							
+						/>
+                        <img
+							src={obensonWork}
+							className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
+							
+						/>
+                       
+					</div>
+				
+		
+		</div>
+	);
+};
 
-                    <div className="cards">
-                        <img src={obensonWork2} alt="" />
-                        <Link to='/project'><button>See More</button></Link>
-                    </div>
-
-                    <div className="cards">
-                        <img src={obensonWork2} alt="" />
-                        <Link to='/project'><button>See More</button></Link>
-                    </div>
-
-                </div>
-            </div></>
-    
-    )
-}
-
-export default Portfolio
+export default Portfolio;
