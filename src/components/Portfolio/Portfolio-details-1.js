5badd516710e32b9c806669233eae5e302d6f0eb
Terogene Claudio <cterogene@gmail.com>
2022-10-04 17:46:36 -0500

    Now on the specific portfolio pages to setup the details about each project

--- src/components/Portfolio/Portfolio-details-1.js
+++ src/components/Portfolio/Portfolio-details-1.js
@@ -1,49 +1,44 @@
 import './Portfolio-details-1.scss'
 import React from 'react'
-import obensonWork from "../../assets/images/obenson-portfolio.jpg"
-import obensonWork2 from "../../assets/images/obenson-work2.png"
-import obensonWork3 from "../../assets/images/obenson-work3.png"
+import obensonWork1 from "../../assets/images/obenson-portfolio.jpg"
+import obensonWork from "../../assets/images/obenson-portfolio-1.png"
+import obensonWork2 from "../../assets/images/obenson-portfolio-2.png"
 
 
 import { Link } from "react-router-dom";
 
 const Portfolio_Details_1 = () => {
-    return ( 
-        <>
-         <div className='project'>
-             <h1>Personal Portfolio</h1>
-            
-        </div>
-        <div className='works'>
-                <div className="work__section">
-        
-                    <div className="cards">
-                        <img src={obensonWork} alt="" />
-                        <Link to='/project'><button>See More</button></Link>
-                    </div>
+    return (
+		<>
+			<div className='header-text'>
+				<h3 className="text-2xl sm:text-2xl text-primary-dark dark:text-primary-light mb-5">
+					PERSONAL PORTFOLIO
+                    </h3>
+			</div>
+			<div className="grid grid-cols-1 gap-3 lg:grid-cols-3 sm:grid-cols-2">
+				
+				<img
+					className="rounded-xl object-cover w-full border shadow-lg"
+					
+					src={obensonWork}
+				/>
 
-                    <div className="cards">
-                        <img src={obensonWork2} alt="" />
-                        <Link to='/project'><button>See More</button></Link>
-                    </div>
+				<img
+					className="rounded-xl object-cover w-full border"
+					src={obensonWork1}
+				/>
 
-                    <div className="cards">
-                        <img src={obensonWork3} alt="" />
-                        <Link to='/project'><button>See More</button></Link>
-                    </div>
+				
+				<img
+					className="rounded-xl object-cover w-full border"
+					src={obensonWork2}
+				/>
 
-                </div>
-            </div>
+			</div>
+			
+		</>
 
-            <div className='description'>
-                        <p>Personal Portfolio of obenson Androuin. I used Asp.net and Bootstrap to code and design the app.</p>
-                        <p>For the hosting, the service was from <a href='https://somee.com' target='_blank'>somee.com</a> and it was a success with the client.</p>
-                        <p><a href='https://github.com/cterogene/ObensonPortfolio/' target='_blank'>project Github link</a> |  <a href='http://garizim.somee.com/' target='_blank'>Website</a></p>
-                    </div></>
-
-
-    
-    )
+	);
 }
 
 export default Portfolio_Details_1
--- src/components/Portfolio/Portfolio-details-2.js
+++ src/components/Portfolio/Portfolio-details-2.js
@@ -1,13 +1,46 @@
 import './Portfolio-details-2.scss'
 import React from 'react'
-import obensonWork from "../../assets/images/obenson-portfolio.jpg"
-import obensonWork2 from "../../assets/images/obenson-work2.png"
-import obensonWork3 from "../../assets/images/obenson-work3.png"
+import obensonWork from "../../assets/images/panier-mobile-1.png"
+import obensonWork1 from "../../assets/images/panier-mobile-2.png"
+import obensonWork2 from "../../assets/images/panier-mobile-3.png"
 
 
 import { Link } from "react-router-dom";
 
 const Portfolio_Details_2 = () => {
+
+    return (
+		<>
+			<div className='header-text'>
+				<h3 className="text-2xl sm:text-2xl text-primary-dark dark:text-primary-light mb-5">
+					PANIER MOBILE 2022
+                    </h3>
+			</div>
+			<div className="grid grid-cols-1 gap-3 lg:grid-cols-3 sm:grid-cols-2">
+				
+				<img
+					className="rounded-xl object-cover w-full border shadow-lg"
+					
+					src={obensonWork}
+				/>
+
+				<img
+					className="rounded-xl object-cover w-full border"
+					src={obensonWork1}
+				/>
+
+				
+				<img
+					className="rounded-xl object-cover w-full border"
+					src={obensonWork2}
+				/>
+
+			</div>
+			
+		</>
+
+	);
+    /*
     return ( 
         <>
          <div className='project'>
@@ -44,6 +77,7 @@ const Portfolio_Details_2 = () => {
 
     
     )
+    */
 }
 
 export default Portfolio_Details_2
